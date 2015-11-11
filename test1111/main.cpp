4b3f9659e4d80546ab0ef188fa5c4b55dc4a778e
ciecclm <clm@imnu.edu.cn>
2015-11-11 18:51:46 +0800

    Initial Commit

---
+++ test1111/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test1111
+//
+//  Created by 朝力萌 on 15/11/11.
+//  Copyright © 2015年 朝力萌. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
